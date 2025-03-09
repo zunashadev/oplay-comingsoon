946bdc728c86f4109d45a575559f60618c91a970
Zun Asha <zunasha080602@gmail.com>
2025-03-09 17:05:09 +0700

    update

--- resources/views/coming-soon.blade.php
+++ resources/views/coming-soon.blade.php
@@ -12,7 +12,9 @@
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
     <!-- Styles / Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+    <link rel="stylesheet" href="{{ asset('build/assets/app-Cf7vOOcw.css') }}">
+    <link rel="stylesheet" href="{{ asset('build/assets/app-BnSuhvBk.js') }}">
 </head>
 
 <body class="h-screen overflow-hidden bg-me-5 flex justify-center items-center">
