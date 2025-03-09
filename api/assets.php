5eb1d59fc05f7ebfe162c026548ae878b8c8919d
Zun Asha <zunasha080602@gmail.com>
2025-03-09 17:29:54 +0700

    update

--- api/assets.php
+++ api/assets.php
@@ -6,8 +6,8 @@
  */
 if ($_GET['type'] === 'css') {
     header("Content-type: text/css; charset: UTF-8");
-    echo require __DIR__ . '/../public/css/' . basename($_GET['file']);
+    echo require __DIR__ . '/../public/build/assets/' . basename($_GET['file']);
 } else if ($_GET['type'] === 'js') {
     header('Content-Type: application/javascript; charset: UTF-8');
-    echo require __DIR__ . '/../public/js/' . basename($_GET['file']);
+    echo require __DIR__ . '/../public/build/assets/' . basename($_GET['file']);
 }
